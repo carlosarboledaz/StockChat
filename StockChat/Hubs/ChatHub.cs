84172fd3f377db761add6332c9f1641766e8ce65
Carlos Arboleda <carlos.arboleda@perficient.com>
2023-01-23 17:53:14 -0500

    Fix an error when loggin out

--- StockChat/Hubs/ChatHub.cs
+++ StockChat/Hubs/ChatHub.cs
@@ -15,6 +15,9 @@ namespace StockChat.Hubs
 
         public async Task SendMessage(string user, string message, string userId)
         {
+
+            //validate the format of the message. If it's for the command, call RabbitMQ to process the API call
+
             await Clients.All.SendAsync("ReceiveMessage", user, message);
             //Save messages on the database
             Message messageObject = new Message();
@@ -24,6 +27,16 @@ namespace StockChat.Hubs
             await _context.Messages.AddAsync(messageObject);
             await _context.SaveChangesAsync();
         }
+
+        //public override async Task OnConnectedAsync()
+        //{
+        //    await Clients.Others.SendAsync("ReceiveNotification", $"{Context.User?.Identity?.Name} join chat");
+        //}
+
+        //public override async Task OnDisconnectedAsync(Exception exception)
+        //{
+        //    await Clients.Others.SendAsync("ReceiveNotification", $"{Context.User?.Identity?.Name} left chat");
+        //}
 
     }
 }
